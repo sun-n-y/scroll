de09b0925f6d96e6cdd74b3aa645963285a8542d
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-01 18:07:11 -0400

    add scroll hero

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const navBtn = document.querySelector('.nav-btn');
 const links = document.querySelector('.links');
 const linksContainer = document.querySelector('.links-container');
+const linksHeight = links.getBoundingClientRect().height;
 
 navBtn.addEventListener('click', () => {
-  const linksHeight = links.getBoundingClientRect().height;
   const containerHeight = linksContainer.getBoundingClientRect().height;
   if (containerHeight === 0) {
     linksContainer.style.height = `${linksHeight}px`;
