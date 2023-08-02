7930e008517f739e2c41bf417049ed563c32157f
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-01 23:41:59 -0400

    add scroll complete

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const navBtn = document.querySelector('.nav-btn');
 const links = document.querySelector('.links');
 const linksContainer = document.querySelector('.links-container');
 const linksHeight = links.getBoundingClientRect().height;
+const topLink = document.querySelector('.top-link');
+const date = document.querySelector('#date');
+
+date.textContent = new Date().getFullYear();
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 65) {
     navbar.classList.add('fixed-nav');
   } else {
     navbar.classList.remove('fixed-nav');
+  }
+  if (window.scrollY > 500) {
+    topLink.classList.add('show-link');
+  } else {
+    topLink.classList.remove('show-link');
   }
 });
 
@@ -25,7 +34,14 @@ links.addEventListener('click', (e) => {
   e.preventDefault();
   const linkName = e.target.getAttribute('href').slice(1);
   const element = document.getElementById(linkName);
-  let yValue = element.offsetTop;
-  console.log(yValue);
-  window.scrollTo(0, yValue);
+  const navbarHeight = navbar.getBoundingClientRect().height;
+  let position = element.offsetTop - navbarHeight;
+  if (!navbar.classList.contains('fixed-nav')) {
+    position = position - navbarHeight;
+  }
+  if (linksHeight > 100) {
+    position = position + linksHeight;
+    linksContainer.style.height = 0;
+  }
+  window.scrollTo(0, position);
 });
