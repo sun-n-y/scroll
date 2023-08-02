bca12ae2b57e886448dafd8b930277bed04a1dcf
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-01 22:46:45 -0400

    add scroll fix transparency

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+const navbar = document.querySelector('.navbar');
 const navBtn = document.querySelector('.nav-btn');
 const links = document.querySelector('.links');
 const linksContainer = document.querySelector('.links-container');
 const linksHeight = links.getBoundingClientRect().height;
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 65) {
+    navbar.classList.add('fixed-nav');
+  } else {
+    navbar.classList.remove('fixed-nav');
+  }
+});
 
 navBtn.addEventListener('click', () => {
   const containerHeight = linksContainer.getBoundingClientRect().height;
@@ -10,4 +19,13 @@ navBtn.addEventListener('click', () => {
   } else {
     linksContainer.style.height = 0;
   }
+});
+
+links.addEventListener('click', (e) => {
+  e.preventDefault();
+  const linkName = e.target.getAttribute('href').slice(1);
+  const element = document.getElementById(linkName);
+  let yValue = element.offsetTop;
+  console.log(yValue);
+  window.scrollTo(0, yValue);
 });
